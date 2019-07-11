42cbed1d0b22c0ad7cb18acb040d6eaa49c1d6b1
Will McLean <willmcl@gmail.com>
2019-07-11 14:43:12 +1000

    Close #2. Update the Image component to accomodate uploadcare images.

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ A starter template for Gatbsy and Netlify CMS web builds by Will McLean
 1. `git init`
 1. Push to github repo
 1. Link Netlify site to github repo for deployment
-1. Setup Netlify Identity
+1. Setup Netlify Identity within Settings > Identity. 
+	1. Choose open or invite for new users
+	1. Add any external providers you want to allow people to log in with
+	1. Enable Git Gateway
+	1. If you have set invite for new users then invite yourself within the Identity tab and follow link in the email you get sent
+	1. If you have set to open the visit siteurl.com/admin to login
 1. Start deving with `gatsby develop`
 
 ## Join a site
--- src/components/atoms/Image.js
+++ src/components/atoms/Image.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import { StaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import React, { Component } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-const Image = ({ imgName }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allImageSharp {
-          edges {
-            node {
-              fluid(maxWidth: 500) {
-                ...GatsbyImageSharpFluid
-                originalName
+class Image extends Component {
+  render() {
+    if( this.props.imgName.includes('ucarecdn') ) {
+      return (
+        <img alt="Will McLean Art"
+          src={`${this.props.imgName}-/resize/1000x/fallback.jpg`}
+          srcSet={`${this.props.imgName}-/resize/640x/640.jpg 640w,
+               ${this.props.imgName}-/resize/1000x/-/quality/lighter/1000.jpg 1000w,
+               ${this.props.imgName}-/resize/1600x/-/quality/lighter/1600.jpg 1600w,`}
+          sizes={this.props.sizes}/>
+      );
+    } else {
+      return (
+        <StaticQuery
+          query={graphql`
+          query {
+            allImageSharp {
+              edges {
+                node {
+                  fluid(maxWidth: 500) {
+                    ...GatsbyImageSharpFluid
+                    originalName
+                  }
+                }
               }
             }
           }
-        }
-      }
-    `}
-    render={data => {
-      const image = data.allImageSharp.edges.find(
-        edge => edge.node.fluid.originalName === imgName
-      );
-      if (!image) {
-        return null
-      }
-      return <Img fluid={image.node.fluid} />
-    }}
-  />
-);
+        `}
+          render={data => {
+            const image = data.allImageSharp.edges.find(
+              edge => edge.node.fluid.originalName === this.props.imgName.replace( /^.*[\\/]/, '' )
+            );
+            if ( !image ) {
+              return null
+            }
+            return <Img fluid={image.node.fluid}/>
+          }}
+        />
+      )
+    }
+  }
+}
 
 export default Image;
