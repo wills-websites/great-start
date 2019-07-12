9ed82ad6c602b09dddd6a96c0f6bc34aaf41cdf4
Will McLean <willmcl@gmail.com>
2019-07-12 13:14:26 +1000

    Close #3. Add markdown converter atom.

---
+++ src/components/atoms/MarkdownConverter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import showdown from 'showdown'
+
+const converter = new showdown.Converter({simpleLineBreaks: true});
+
+class MarkdownConverter extends Component {
+    render() {
+        return (
+          <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(this.props.content)}}/>
+        )
+    }
+}
+
+export default MarkdownConverter;
