538248fef0254055894d065872793099b1854b3c
Will McLean <willmcl@gmail.com>
2022-02-11 14:12:17 +0000

    Close #75. Add a default fontface setup.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
+// import '../utils/fontface.css';
 
 import Header from "../components/molecules/Header";
 import Footer from "../components/molecules/Footer";
@@ -7,25 +8,23 @@ import Navigation from "../components/molecules/Navigation";
 import Transition from "../components/atoms/Transition";
 
 import GlobalStyles from "../components/atoms/GlobalStyles";
-import { ThemeProvider } from "styled-components";
-import { theme } from "../utils/styling";
+import {ThemeProvider} from "styled-components";
+import {theme} from "../utils/styling";
 
-class Index extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyles/>
-          <Header/>
-          <Navigation/>
-          <Transition location={this.props.location}>
-            <main>{this.props.children}</main>
-          </Transition>
-          <Footer/>
-        </>
-      </ThemeProvider>
-    );
-  }
+function Index({location, children}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Navigation />
+        <Transition location={location}>
+          <main>{children}</main>
+        </Transition>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
 }
 
 Index.propTypes = {
