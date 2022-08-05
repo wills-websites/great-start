54fd64e3288f2c31ae6970246cd9564918ab53e7
Olly Evans <ollydevans@gmail.com>
2022-08-05 17:47:56 +1000

    Added more conversions - convertToClass & formatPhoneString

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,16 @@ export const convertToSlug = (Text) => {
       .replace(/ +/g, "-")
   )
 }
+
+export const convertToClass = (Text) => {
+  return (
+      Text.toLowerCase()
+          .replace(/[^\w ]+/g, "")
+          .replace(/ +/g, "-")
+  )
+}
+
+export const formatPhoneString = (Text) => {
+  return Text.toLowerCase()
+      .replace(/[\s.]+/g, "")
+}
