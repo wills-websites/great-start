061e39867a38e39b2dbad0b3376379007c999a71
Will McLean <willmcl@gmail.com>
2023-05-19 16:12:17 +0100

    Add projects to the link provider.

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -2,7 +2,7 @@ const linkResolver = (doc) => {
 
   // Pretty URLs for known types
   // if (doc.type === 'news') return "/news/" + doc.uid;
-  // if (doc.type === 'project') return "/projects/" + doc.uid;
+  if (doc.type === 'project') return "/projects/" + doc.uid;
   // if (doc.type === 'page') return "/" + doc.uid;
 
   // Backup for all other types
