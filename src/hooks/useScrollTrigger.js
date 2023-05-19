cd061700687feded2542fb2759ce0b837136768a
Will McLean <willmcl@gmail.com>
2023-05-19 10:23:36 +0100

    See #100. Add mm to scrolltrigger hook.

--- src/hooks/useScrollTrigger.js
+++ src/hooks/useScrollTrigger.js
@@ -1,12 +1,15 @@
 import {useRef} from 'react';
 import {gsap} from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
 export default function useScrollTrigger() {
   const ref = useRef(null);
   const tl = useRef(null);
   const q = gsap.utils.selector(ref);
+  const mm = gsap.matchMedia()
 
   return {
     holderRef: ref,
@@ -14,5 +17,6 @@ export default function useScrollTrigger() {
     q: q,
     st: ScrollTrigger,
     gsap: gsap,
+    mm: mm,
   };
 };
