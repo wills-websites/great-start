9bc4139849514370de3b7de4d72c8832db483800
Will McLean <willmcl@gmail.com>
2023-05-19 16:31:17 +0100

    Add a project list page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,3 +4,41 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const path = require("path");
+
+exports.createPages = ({actions, graphql}) => {
+  const {createPage} = actions;
+
+  return graphql(`
+      {
+          projects: allPrismicProject(sort: {fields: first_publication_date, order: DESC}) {
+              nodes {
+                  uid
+                  id
+              }
+          }
+      }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    // Create project list pages
+    const projects = result.data.projects.nodes;
+    const projectsPerPage = 20;
+    const numProjectPages = Math.ceil(projects.length / projectsPerPage);
+    Array.from({length: numProjectPages}).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/projects/` : `/projects/${i + 1}`,
+        component: path.resolve('./src/templates/ProjectListPage.js'),
+        context: {
+          limit: projectsPerPage,
+          skip: i * projectsPerPage,
+          numProjectPages,
+          currentPage: i + 1,
+        },
+      })
+    });
+
+  })
+};
---
+++ src/templates/ProjectListPage.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Seo from '../components/molecules/Seo';
+import {graphql, Link} from 'gatsby';
+import styled from 'styled-components';
+import Container from '../components/atoms/Container';
+
+
+const Holder = styled.article`
+`;
+
+
+function ProjectListPage({data}) {
+  const posts = data.allPrismicProject.nodes;
+  return (
+    <Holder>
+      <Seo title="Projects" />
+      <h1>Projects</h1>
+      <Container>
+        <ul>
+          {posts && posts.map(post =>
+            <li key={post.id}>
+              <p><Link to={`/projects/${post.uid}`}>{post.data.title.text}</Link></p>
+            </li>
+          )}
+        </ul>
+      </Container>
+    </Holder>
+  )
+}
+
+export default ProjectListPage
+
+export const projectListPageQuery = graphql`
+    query projectListPageQuery($skip: Int!, $limit: Int!) {
+        allPrismicProject(
+            limit: $limit
+            skip: $skip
+            sort: {first_publication_date: DESC}
+        ) {
+            nodes {
+                uid
+                data {
+                    title {
+                        text
+                    }
+                }
+            }
+        }
+    }
+`;
